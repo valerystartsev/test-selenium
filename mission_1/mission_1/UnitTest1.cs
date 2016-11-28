afeb4099ce60ae32e6620a6bf5a43b3b3e000a57
Valery Startsev <valery.startsev@confirmit.com>
2016-11-28 18:03:42 +0300

    First Test - Seach in Yandex, missed file

---
+++ mission_1/mission_1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace mission_1
+{
+    [TestFixture]
+    public class FirstTestClass
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        [SetUp]
+        public void Init()
+        {
+            _driver = new ChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Test1_SearchInYandex()
+        {
+            _driver.Url = "http://ya.ru";
+            _driver.FindElement(By.Name("text")).SendKeys("webdriver");
+            _driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+            
+            _wait.Until(ExpectedConditions.TitleContains("webdriver — Яндекс: нашлось"));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
